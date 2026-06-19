97f10cc12c64407916c58ec6827f22eb20c5544c
AnuPillalamari <24eg105j38@anurag.edu.in>
2026-06-19 15:55:38 +0530

    fix api base url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://tradeez-backend-j1h9.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
